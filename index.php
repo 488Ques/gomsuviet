30b081894672a497172929e1c27ec616b6e2d01c
Tri <ngminhtri002@gmail.com>
2023-05-24 08:53:28 +0700

    Minor fix

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 require_once($_SERVER['DOCUMENT_ROOT'] . '/include_path.php');
 require_once(DIR_CONTROLLERS . 'index.ctl.php');
 
-echo $_SERVER['DOCUMENT_ROOT'] . '/include_path.php';
-
 $title = 'Trang chủ';
 $template = DIR_VIEWS . 'index.tmpl.php';
 $indexController = new indexController();
