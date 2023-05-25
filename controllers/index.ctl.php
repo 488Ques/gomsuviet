6e8f7e2f956a19374c4689cc608e78724e2e3605
Tri <ngminhtri002@gmail.com>
2023-05-26 02:29:52 +0700

    fix: rewrite index controller to fit with other controllers

--- controllers/index.ctl.php
+++ controllers/index.ctl.php
@@ -3,19 +3,29 @@ require_once('modules/db.php');
 require_once('models/product.php');
 
 class Index {
-    public static function render(): void
+    private static productModel $productModel;
+
+    private static function init(): void
     {
-        $productModel = new productModel(DB());
-        $phones = $productModel->getFourProducts(1);
-        $tablets = $productModel->getFourProducts(2);
-        $laptops = $productModel->getFourProducts(3);
-        $acs = $productModel->getFourProducts(4);
-        $fridges = $productModel->getFourProducts(5);
-        $washers = $productModel->getFourProducts(6);
+        self::$productModel = new productModel(DB());
+    }
+
+    public static function Render(): void
+    {
+        self::init();
+
+        // Initialize template data
+        $phones = self::$productModel->getFourProducts(1);
+        $tablets = self::$productModel->getFourProducts(2);
+        $laptops = self::$productModel->getFourProducts(3);
+        $acs = self::$productModel->getFourProducts(4);
+        $fridges = self::$productModel->getFourProducts(5);
+        $washers = self::$productModel->getFourProducts(6);
 
         $title = 'Trang chủ';
         $template = 'views/index.tmpl.php';
 
+        // Render template
         require_once('views/layout.php');
     }
 }
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 require_once('controllers/index.ctl.php');
 
-Index::render();
+Index::Render();
