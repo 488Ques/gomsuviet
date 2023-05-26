2f0ee283d6f2e5a7d6b61557e3d657e9cc228fe2
Tri <ngminhtri002@gmail.com>
2023-05-27 01:30:25 +0700

    feat: change brand's name

--- views/header.partial.php
+++ views/header.partial.php
@@ -4,7 +4,7 @@ require_once('modules/cart.php');
 
 <nav class="navbar navbar-expand-lg">
     <div class="container">
-        <a class="navbar-brand" href="/">TIENTRIS</a>
+        <a class="navbar-brand" href="/">Gốm Sứ Việt</a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
