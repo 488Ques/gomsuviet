33533c5c4cd26413810bc45a7b82c23873e85e2b
Tri <ngminhtri002@gmail.com>
2023-06-03 01:40:37 +0700

    fix: hide carousel in index page

--- views/index.tmpl.php
+++ views/index.tmpl.php
@@ -16,50 +16,50 @@ $productCard = '
                 ';
 ?>
 
-<div id="productCarousel" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-indicators">
-        <button type="button" data-bs-target="#productCarousel" data-bs-slide-to="0" class="active" aria-current="true"
-                aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#productCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#productCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    </div>
-    <div class="carousel-inner font-SamsungOne darkened">
-        <div class="carousel-item active">
-            <img src="/static/image/index/1.webp" class="d-block w-100">
-            <div class="carousel-caption">
-                <h5>Samsung Galaxy A03</h5>
-                <p>Ưu đãi đến <b>5 triệu</b>, hỗ trợ <b>5 triệu</b> thu cũ đổi mới và giảm thêm <b>1 triệu</b> khi thanh
-                    toán
-                    trước.</p>
-                <p><a href="/detail.php?id=1" class="rounded-pill px-4 btn btn-primary">Mua ngay</a></p>
-            </div>
-        </div>
-        <div class="carousel-item">
-            <img src="/static/image/index/2.webp" class="d-block w-100">
-            <div class="carousel-caption">
-                <h5>Tủ lạnh Panasonic Inverter</h5>
-                <p>Giữ thực phẩm tươi lâu và tiết kiệm điện gấp 2 lần.</p>
-                <p><a href="/detail.php?id=49" class="rounded-pill px-4 btn btn-primary">Mua ngay</a></p>
-            </div>
-        </div>
-        <div class="carousel-item">
-            <img src="/static/image/index/3.webp" class="d-block w-100">
-            <div class="carousel-caption">
-                <h5>Samsung Galaxy Tab A7</h5>
-                <p>Mang đến sự thay đổi hoàn toàn, hoàn thiện tỉ mỉ trong từng đường nét, bền bỉ vượt thời gian.</p>
-                <p><a href="/detail.php?id=17" class="rounded-pill px-4 btn btn-primary">Mua ngay</a></p>
-            </div>
-        </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#productCarousel" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#productCarousel" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
-</div>
+<!--<div id="productCarousel" class="carousel slide" data-bs-ride="carousel">-->
+<!--    <div class="carousel-indicators">-->
+<!--        <button type="button" data-bs-target="#productCarousel" data-bs-slide-to="0" class="active" aria-current="true"-->
+<!--                aria-label="Slide 1"></button>-->
+<!--        <button type="button" data-bs-target="#productCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>-->
+<!--        <button type="button" data-bs-target="#productCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>-->
+<!--    </div>-->
+<!--    <div class="carousel-inner font-SamsungOne darkened">-->
+<!--        <div class="carousel-item active">-->
+<!--            <img src="/static/image/index/1.webp" class="d-block w-100">-->
+<!--            <div class="carousel-caption">-->
+<!--                <h5>Samsung Galaxy A03</h5>-->
+<!--                <p>Ưu đãi đến <b>5 triệu</b>, hỗ trợ <b>5 triệu</b> thu cũ đổi mới và giảm thêm <b>1 triệu</b> khi thanh-->
+<!--                    toán-->
+<!--                    trước.</p>-->
+<!--                <p><a href="/detail.php?id=1" class="rounded-pill px-4 btn btn-primary">Mua ngay</a></p>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="carousel-item">-->
+<!--            <img src="/static/image/index/2.webp" class="d-block w-100">-->
+<!--            <div class="carousel-caption">-->
+<!--                <h5>Tủ lạnh Panasonic Inverter</h5>-->
+<!--                <p>Giữ thực phẩm tươi lâu và tiết kiệm điện gấp 2 lần.</p>-->
+<!--                <p><a href="/detail.php?id=49" class="rounded-pill px-4 btn btn-primary">Mua ngay</a></p>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div class="carousel-item">-->
+<!--            <img src="/static/image/index/3.webp" class="d-block w-100">-->
+<!--            <div class="carousel-caption">-->
+<!--                <h5>Samsung Galaxy Tab A7</h5>-->
+<!--                <p>Mang đến sự thay đổi hoàn toàn, hoàn thiện tỉ mỉ trong từng đường nét, bền bỉ vượt thời gian.</p>-->
+<!--                <p><a href="/detail.php?id=17" class="rounded-pill px-4 btn btn-primary">Mua ngay</a></p>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--    <button class="carousel-control-prev" type="button" data-bs-target="#productCarousel" data-bs-slide="prev">-->
+<!--        <span class="carousel-control-prev-icon" aria-hidden="true"></span>-->
+<!--        <span class="visually-hidden">Previous</span>-->
+<!--    </button>-->
+<!--    <button class="carousel-control-next" type="button" data-bs-target="#productCarousel" data-bs-slide="next">-->
+<!--        <span class="carousel-control-next-icon" aria-hidden="true"></span>-->
+<!--        <span class="visually-hidden">Next</span>-->
+<!--    </button>-->
+<!--</div>-->
 
 
 <div class="container">
